a4a1b0959fb012ba2857a79b2551cc0876c7a07a
ravi <your-public-email@example.com>
2025-02-27 12:18:50 +0530

    aws providers_aws

---
+++ Providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = { amazon web
+      source = "hashicorp/aws"
+      version = "5.88.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
